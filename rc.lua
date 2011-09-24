65505a89bcdec07eedc72846a975e24bb74942cf
Jarod Lau <jarodlau@gmail.com>
2011-09-25 02:45:35 +0800

    change to the default theme

--- rc.lua
+++ rc.lua
@@ -15,7 +15,7 @@ require("menu")
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
